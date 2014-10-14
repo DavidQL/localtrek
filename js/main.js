48a2442439392d8dd61e532b733508ad4f15b04d
David Aragon <david@quickleft.com>
2014-10-14 13:28:33 -0600

    add missing assets

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+  $(document).on('click', 'a.sign-up', function(e) {
+    e.preventDefault();
+    var email = $('.email').val();
+    $.ajax({
+        url: "https://docs.google.com/forms/d/1Wi16pSVc83ynlbYJtkMjSPG_bfdcSUL1O4eRdZPg8OA/formResponse",
+        data: {"entry.1653288301" : email},
+        type: "POST",
+        dataType: "xml",
+        statusCode: {
+            0: function (){
+                $('.email').val("");
+                vex.dialog.alert("Thanks! We'll be in touch.");
+            },
+            200: function (){
+                $('.email').val("");
+                vex.dialog.alert("Thanks! We'll be in touch.");
+            }
+        }
+    });
+  });
+});
