3f71cff24bcd6077ce033239168cb3899a12c4e1
David Aragon <david@quickleft.com>
2014-12-02 16:34:13 -0700

    fetch the list of paths from amazon

--- js/path-read.js
+++ js/path-read.js
@@ -2,9 +2,28 @@ var db = {
   init: function(callback) {
     AWS.config.update({accessKeyId: 'AKIAIPCCCSQMN5WXBBSA', secretAccessKey: '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', region: 'us-west-2'});
     db.instance = new AWS.DynamoDB();
+    callback && callback();
   },
   getPaths: function() {
-    
+    db.instance.scan({
+      'Limit': 10,
+      'TableName': 'PerfectPath'
+    }, function(err, data) {
+      if (err) {
+        console.log('error', err);
+        return;
+      }
+      data.Items.forEach(function(item) {
+        if (item.title) {
+          $('<li/>')
+          .append($('<a/>', {
+            href: '/path-show?id=' + item.id.S,
+            text: item.title.S
+          }))
+          .appendTo('ul.path-list');
+        }
+      });
+    })
   }
 }
 
